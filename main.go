3378a73c92e4fd415ba79b8cd93d1c976e33f114
Josue Vargas <jvargasprog@gmail.com>
2023-10-11 21:57:41 -0700

    Added options menu and cleaned up code

--- main.go
+++ main.go
@@ -1,24 +1,86 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"math/rand"
-  "fmt"
+	"os"
+	"strconv"
 )
 
 func calcRatio(sampleSize int) int {
-  returnValue := 0
+	returnValue := 0
 
-  for i := 0; i <= sampleSize; i++ {
-    x := rand.Float32()
-    y := rand.Float32()
+	for i := 0; i <= sampleSize; i++ {
+		x := rand.Float32()
+		y := rand.Float32()
 
-    if (x*x + y*y <= 1) {
-      returnValue++
-    }
-  } 
-  return returnValue
+		if x*x+y*y <= 1 {
+			returnValue++
+		}
+	}
+	return returnValue
 }
 
-func main() { 
-  fmt.Println("Sample Size: ")
+func determineSampleSize(s string) int {
+	switch s {
+	case "IntMax":
+		return math.MaxInt32
+	case "Int32":
+		return math.MaxInt32
+	case "Int16":
+		return math.MaxInt16
+	case "Int8":
+		return math.MaxUint8
+	}
+	returnValue, err := strconv.Atoi(s)
+	if err == nil {
+		return returnValue
+	}
+	return 0
+}
+
+func monteCarloSim() {
+	fmt.Print("Sample Size?: ")
+	var i string
+	fmt.Scanln(&i)
+	sampleSize := determineSampleSize(i)
+	sampleTop := calcRatio(sampleSize)
+	piAprox := 4.0 * float32(sampleTop) / float32(sampleSize)
+	fmt.Printf("Pi Aproximated with %v samples: %v\n", sampleSize, piAprox)
+}
+
+func optionsMenu() int {
+	fmt.Print("Options: \nMonte Carlo = 1\nMonte Carlo also (for now...) = 2\nExit = exit\n")
+	fmt.Print("Option? : ")
+	var input string
+	fmt.Scanln(&input)
+
+	switch input {
+	case "1":
+		return 1
+	case "2":
+		return 2
+	case "exit":
+		fmt.Println("Exiting...")
+		os.Exit(1)
+	}
+	return -1
+}
+
+func runOption(input int) {
+	switch input {
+	case 1:
+		monteCarloSim()
+	case 2:
+		monteCarloSim()
+	case -1:
+		fmt.Println("Input error")
+	}
+
+}
+
+func main() {
+	optionValue := optionsMenu()
+	runOption(optionValue)
 }
--- main_test.go
+++ main_test.go
@@ -10,5 +10,14 @@ func TestIntMax(t *testing.T) {
 	i := math.MaxInt32
 	top := float32(calcRatio(i))
 	bottom := float32(i)
-	fmt.Println(4 * top/bottom)
-  }
+  result := 4 * top/bottom
+  fmt.Printf("IntMax result: %v\n", result)
+}
+
+func TestInt8(t * testing.T) {
+  i := math.MaxInt8
+  top := float32(calcRatio(i))
+  bottom := float32(i)
+  result := 4 * top / bottom
+  fmt.Printf("Int8 result: %v\n", result)
+}
